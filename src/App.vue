bd5b324bcde43cca61dcc3bcac1f082a81edcf2f
= <=>
2023-03-03 11:22:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+export default{
+  data(){
+    return {
+      contador: 0
+    }
+  },
+
+methods: {
+    incrementarContador(){
+      this.contador++
+    },
+    decrementarContador(){
+      if (this.contador > 0){
+        this.contador--
+      } else {
+        alert("O contador nao pode ser menor que zero")
+      }
+    },
+    resetarContador(){
+      this.contador = 0;
+    }
+  }
+}
+
+/* a */
+</script>
+
+<template>
+
+<button @click="incrementarContador">+</button>
+<button @click="decrementarContador">-</button>
+<button @click="resetarContador">Resetar</button>
+o contador é {{ contador }}
+
+</template>
+  
+<style scoped>
+
+button{
+  font-weight: bold;
+}
+
+</style>
